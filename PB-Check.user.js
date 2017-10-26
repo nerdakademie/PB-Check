43a34e1dc369640aea3a874cacb5b9d941adcb03
Finja Hauschild <finja.hauschild@storecast.de>
2017-10-26 21:07:48 +0200

    changed variable names because of script collision

--- PB-Check.user.js
+++ PB-Check.user.js
@@ -3,7 +3,7 @@
 // @namespace   nak
 // @description checks for your PB to be accepted
 // @include     https://cis.nordakademie.de/studium/bachelor/transferleistungen-praxisberichte/
-// @version     0.1
+// @version     0.1.1
 // @grant       none
 // @downloadURL https://github.com/nerdakademie/PB-Check/raw/master/PB-Check.user.js
 // @updateURL   https://github.com/nerdakademie/PB-Check/raw/master/PB-Check.meta.js
@@ -13,28 +13,28 @@ function reloadPage(){
     location.reload();
 }
 
-var oldTableContent = localStorage.getItem("oldTable");
-var oldTableArray = JSON.parse(localStorage.getItem("oldTableArray"));
-var compareTable = document.getElementsByClassName("table");
-var compareTableContent = compareTable[0].textContent;
-var rowCount = compareTable[0].rows.length;
+var thatOldTableContent = localStorage.getItem("thatOldTableContent");
+var thatOldTableArray = JSON.parse(localStorage.getItem("thatOldTableArray"));
+var comparisonTable = document.getElementsByClassName("table");
+var comparisonTableContent = comparisonTable[0].textContent;
+var rowCounter = comparisonTable[0].rows.length;
 
 
 function comparing() {
-    if (compareTableContent != oldTableContent) {
+    if (comparisonTableContent != thatOldTableContent) {
       var paper = "", array = new Array();
-      if(oldTableArray === null){
-        oldTableArray = new Array();
+      if(thatOldTableArray === null){
+        thatOldTableArray = new Array();
       }
-      for (var i = 1, row; row = compareTable[0].rows[i]; i++) {
-        if(i < compareTable[0].rows.length){
-          if(oldTableArray.length > i ){
-              if(oldTableArray[i][1] !== row.cells[5].title.trim() && row.cells[5].title.trim() === "akzeptiert"){
+      for (var i = 1, row; row = comparisonTable[0].rows[i]; i++) {
+        if(i < comparisonTable[0].rows.length){
+          if(thatOldTableArray.length > i ){
+              if(thatOldTableArray[i][1] !== row.cells[5].title.trim() && row.cells[5].title.trim() === "akzeptiert"){
               // PB paper status has changed
               array.push([row.cells[0].textContent.trim() , row.cells[5].title.trim()]);
             }
           }
-          oldTableArray[i] = [row.cells[0].textContent.trim() , row.cells[5].title.trim()];
+          thatOldTableArray[i] = [row.cells[0].textContent.trim() , row.cells[5].title.trim()];
         }
       }
       for(var j = 0, len = array.length; j < len; j++){
@@ -49,11 +49,11 @@ function comparing() {
         alert('Deine Transferleistung ' + paper + ' wurde akzeptiert!');
       }
       var oldTable = document.getElementsByClassName("table");
-      oldTableContent = oldTable[0].textContent;
-      localStorage.setItem("oldTable", oldTableContent);
-      localStorage.setItem("oldTableArray", JSON.stringify(oldTableArray));
+      thatOldTableContent = oldTable[0].textContent;
+      localStorage.setItem("thatOldTableContent", thatOldTableContent);
+      localStorage.setItem("thatOldTableArray", JSON.stringify(thatOldTableArray));
     }
-    compareTable[0].rows[rowCount-1].cells[4].textContent = average(oldTableArray);
+    comparisonTable[0].rows[rowCounter-1].cells[4].textContent = average(thatOldTableArray);
     setTimeout(reloadPage,60000);
 }
 
